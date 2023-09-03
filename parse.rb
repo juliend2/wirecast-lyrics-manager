cee70b34df7bdbb0ac660c0d6fd977f46186a47b
Julien Desrosiers <juliend2@gmail.com>
2023-09-03 11:24:34 -0400

    decode the attribute to get the plain text

--- parse.rb
+++ parse.rb
@@ -1,11 +1,18 @@
 require 'rexml/document'
-puts 'joie'
+require 'json'
+require 'base64'
+require 'uri'
 
 include REXML
 
 file = File.new('messe.xml')
 xmldoc = Document.new(file)
 
-XPath.each(xmldoc, "//source") do |source|
-  puts source
+XPath.each(xmldoc, "//source/xml_tag[@widget_settings]") do |source|
+  enc = JSON.parse(source['widget_settings'])['text']
+  if enc
+    plain = Base64.decode64(enc)
+    text = URI.decode(plain)
+    puts text
+  end
 end
