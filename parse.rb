985fe56eeb20b45b8411f77a769fdaa03fe879ed
Julien Desrosiers <juliend2@gmail.com>
2023-09-24 12:14:47 -0400

    grep by text to find title

--- parse.rb
+++ parse.rb
@@ -1,3 +1,8 @@
+# Example usage:
+#
+# ruby parse.xml 'la force'
+# > ... results ...
+
 require 'rexml/document'
 require 'json'
 require 'base64'
@@ -8,11 +13,30 @@ include REXML
 file = File.new('messe.xml')
 xmldoc = Document.new(file)
 
-XPath.each(xmldoc, "//source/xml_tag[@widget_settings]") do |source|
-  enc = JSON.parse(source['widget_settings'])['text']
-  if enc
-    plain = Base64.decode64(enc)
-    text = URI.decode(plain)
-    puts text
+grep = ARGV.shift
+
+grepped = []
+
+grepped_title = 'NOT FOUND'
+grepped_text = 'N/A'
+
+XPath.each(xmldoc, "//source") do |source|
+  next if source['groupGUID'] != 'F091FF4F-1F10-4203-8854-05F8B0414BC0'
+  title = source['prettyname']
+  text = ''
+  XPath.each(source, "xml_tag[@widget_settings]") do |ws|
+    json = JSON.parse(ws['widget_settings'])
+    enc = json['text']
+    if enc
+      plain = Base64.decode64(enc)
+      text = URI.decode(plain)
+      if text =~ /#{grep}/
+        grepped << {title: title, text: text}
+      end
+    end
   end
+
 end
+
+puts grepped.map{|g| g[:title] }
+
