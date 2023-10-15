3973d48cb2ea4ca1c62b044b0b8c1691b4362d69
Julien Desrosiers <juliend2@gmail.com>
2023-10-15 12:37:26 -0400

    wip

--- parse.rb
+++ parse.rb
@@ -20,23 +20,44 @@ grepped = []
 grepped_title = 'NOT FOUND'
 grepped_text = 'N/A'
 
-XPath.each(xmldoc, "//source") do |source|
-  next if source['groupGUID'] != 'F091FF4F-1F10-4203-8854-05F8B0414BC0'
-  title = source['prettyname']
-  text = ''
-  XPath.each(source, "xml_tag[@widget_settings]") do |ws|
-    json = JSON.parse(ws['widget_settings'])
-    enc = json['text']
-    if enc
-      plain = Base64.decode64(enc)
-      text = URI.decode(plain)
-      if text =~ /#{grep}/
-        grepped << {title: title, text: text}
-      end
-    end
-  end
 
+shots = {}
+XPath.each(xmldoc, '//shot') do |shot|
+  #puts 'unique id'
+  #puts shot['unique_id']
+  unique_id = ''
+  pp shot.elements['child']
+#   XPath.each(shot, '//event/event') do |event|
+#     unique_id = event['unique_id']
+#     puts unique_id
+#   end
+#   if unique_id != ''
+#     shot[ unique_id ] = shot['unique_id'] 
+#   end
 end
+#pp shots
 
-puts grepped.map{|g| g[:title] }
+# XPath.each(xmldoc, "//source") do |source|
+#   next if source['groupGUID'] != 'F091FF4F-1F10-4203-8854-05F8B0414BC0'
+#   # FIXME:
+#   source['unique_id'] # UUID that points to a <shot>,
+#   title = source['prettyname'] # not good. it's the Name of the element containing the text
+# 
+#   text = ''
+#   XPath.each(source, "xml_tag[@widget_settings]") do |ws|
+#     json = JSON.parse(ws['widget_settings'])
+#     enc = json['text']
+#     if enc
+#       plain = Base64.decode64(enc)
+#       text = URI.decode(plain)
+#       if text =~ /#{grep}/
+#         puts source
+#         grepped << {title: title, text: text}
+#       end
+#     end
+#   end
+# 
+# end
+
+# puts grepped.map{|g| g[:title] }
 
